f913162bb0fe12933b24a89740b243deaec192c0
Caroline Melo <84986038+CarolineFMelo@users.noreply.github.com>
2022-09-27 23:42:09 -0300

    iniciando galeria

---
+++ src/Components/Sidebar/index.js
@@ -0,0 +1,19 @@
+import "./style.css";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <a href="">2022</a>
+            <a href="">2021</a>
+            <a href="">2020</a>
+            <a href="">2019</a>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+        </div>
+    )
+};
+
+export default Sidebar;
