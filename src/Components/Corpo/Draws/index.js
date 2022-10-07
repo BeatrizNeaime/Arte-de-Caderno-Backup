27e9edccefde5c6a8d6d23cdbdc9524eca03d3af
Caroline Melo <84986038+CarolineFMelo@users.noreply.github.com>
2022-10-07 15:43:29 -0300

    finalizando frontend galery

---
+++ src/Components/Corpo/Draws/index.js
@@ -0,0 +1,20 @@
+import "./style.css";
+
+function Draws() {
+
+    var logo = require('../../../assets/img/visual.png')
+
+    return (
+        <div>
+            <div class="gallery">
+                <a target="_blank" href="">
+                    <img src={logo} alt="Forest" width="600" height="400"></img>
+                </a>
+            </div>
+            <div class="desc">Autor: Nome Completo</div>
+            <div class="desc">Info Adicional</div>
+        </div>
+    )
+};
+
+export default Draws;
--- src/Galery.js
+++ src/Galery.js
@@ -1,15 +1,41 @@
-import './App.css';
 import Navbar from './Components/Navbar';
 import PageTitle from './Components/Corpo/PageTitle';
 import Sidebar from './Components/Sidebar';
+import Draws from './Components/Corpo/Draws';
+import "./Galery.css";
+
 function Galery() {
   return (
-    <div className="Galery">
+    <div className="galery">
       <Navbar />
       <PageTitle 
         title = 'galeria'
       />
-      <Sidebar />
+      <div className='body-galery'>
+        <Sidebar />
+        <div className='draws'>
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+          <Draws />
+        </div>
+      </div>
     </div>
   );
 }
